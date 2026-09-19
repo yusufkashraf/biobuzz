a0f4d7dc09428b52dcc2dcc74e89b22d6840d66e
Yusuf Kamal Ashraf <yusufkamalashraf@gmail.com>
2026-09-19 12:41:43 -0600

    Create mecanum tuner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/TeleOperation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/TeleOperation.java
@@ -18,9 +18,9 @@ public class TeleOperation extends LinearOpMode {
 
         while (opModeIsActive()) {
 
-            /**
-             * free will! we can choose robot or field centric!
-             */
+
+            //free will! we can choose robot or field centric!
+
             //robot.drivetrain.robotCentricDrive(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x);
             robot.drivetrain.fieldCentricDrive(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x);
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
@@ -1,5 +1,14 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
+import com.pedropathing.tuning.autotune.Procedure;
+import com.pedropathing.tuning.autotune.Tuner;
+
+import org.firstinspires.ftc.teamcode.pedro.procedures.MecanumTuner;
+
 public class Tuning {
     // Tuners go here
+    @Tuner
+    public static Procedure mecanumTuner() {
+        return new MecanumTuner();
+    }
 }
