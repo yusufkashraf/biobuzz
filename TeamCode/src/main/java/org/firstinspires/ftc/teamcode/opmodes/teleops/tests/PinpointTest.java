e177f013dd89d4fa8bf1c8be62a4189c8412e546
Yusuf Kamal Ashraf <yusufkamalashraf@gmail.com>
2026-09-24 22:52:05 -0600

    Create Pinpoint testing opmode with telemtry and robot-centric drive. Placed in group "tests"

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/tests/PinpointTest.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleops.tests;
+
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+@TeleOp(group = "tests")
+public class PinpointTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        DcMotorEx frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        DcMotorEx frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        DcMotorEx backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+        DcMotorEx backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+
+        GoBildaPinpointDriver pinpoint =
+                hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            pinpoint.update();
+
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x * 1.1;
+            double rx = gamepad1.right_stick_x;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
+
+            if (gamepad1.share) {
+                pinpoint.setPosition(new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.DEGREES, 0));
+            }
+
+            telemetry.addData("X", "%.2f in",
+                    pinpoint.getPosX(DistanceUnit.INCH));
+
+            telemetry.addData("Y", "%.2f in",
+                    pinpoint.getPosY(DistanceUnit.INCH));
+
+            telemetry.addData("Heading", "%.2f°",
+                    pinpoint.getHeading(AngleUnit.DEGREES));
+
+            telemetry.update();
+        }
+    }
+}
